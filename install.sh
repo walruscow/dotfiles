ba7fd3841666cafa7457f41c9aa16ff564223c69
walruscow <willgmcd@gmail.com>
2023-04-04 23:08:08 +0000

    fix install.sh running from zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 for f in aliases bashrc complete-aliases.sh complete-git.sh gitconfig vimrc zshrc; do
   if ! [[ $(realpath ~/.$f) == $(realpath $f) ]]; then
     mv ~/.$f ~/.$f.bk &>/dev/null
-    ln -s $(pwd)/$f ~/.$f
+    ln -fs $(pwd)/$f ~/.$f
   fi
 done
