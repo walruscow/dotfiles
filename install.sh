362cfd4b70b2f7b085d125b55a5e65c980f0deb1
William McDonald <william.mcdonald@discordapp.com>
2023-05-30 21:34:34 +0000

    add tmux.conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in aliases bashrc complete-aliases.sh complete-git.sh gitconfig vimrc zshrc; do
+for f in aliases bashrc complete-aliases.sh complete-git.sh gitconfig vimrc zshrc tmux.conf; do
   if ! [[ $(realpath ~/.$f) == $(realpath $f) ]]; then
     mv ~/.$f ~/.$f.bk &>/dev/null
     ln -fs $(pwd)/$f ~/.$f
